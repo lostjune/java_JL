74d2f6c90718eaff75931f024970ba9c41b52949
java_JL <lostjune@naver.com>
2021-10-27 17:37:52 +0900

    1-5 문자열 선언 예제(2021.10.27)

---
+++ JL/src/day1/StringEx1문자열선언예제05.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class StringEx1문자열선언예제05 {
+
+	public static void main(String[] args) {
+		/* 문자열 String
+		 * */ 
+		String str1;
+		String str2 = "Hello";
+		System.out.println(str2);
+		str1 = str2 + " world!";
+		System.out.println(str1);
+		
+	}
+
+}
