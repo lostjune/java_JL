1e3b6e7d60d232c336c7aedf0a1921b1ae6eaafd
lostjune <93233438+lostjune@users.noreply.github.com>
2021-10-28 17:52:13 +0900

    14. IfNumEx1 (2021.10.28)

---
+++ JL/src/day2/IfNumEx114.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class IfNumEx114 {
+
+	public static void main(String[] args) {
+		/* (num가 0보다 크)면 [양수라고 출력]하고
+		 * (num가 0이)면 [0이라고 출력]하고
+		 * (num가 0보다 작으)면 [음수라고 출력]하는 코드를 작성하세요.
+		 */
+		int num = 0;
+		if(num > 0) {
+			System.out.println(num + "는 양수");
+		}
+		else if(num == 0) {
+			System.out.println(num);
+		}else if(num < 0) {
+			System.out.println(num + "는 음수");
+		}
+	}
+	//정수가 0보다 크지않고, 정수가 0보다 같은 않은 수 = 음수
+}
