3dcb53eee88f70739b1ec2ecb440eb1e9a234cb9
java_JL <lostjune@naver.com>
2021-10-29 17:41:44 +0900

    ForNumberEx1숫자반복문12 (2021.10.29)

---
+++ JL/src/day3/ForNumberEx1숫자반복문12.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class ForNumberEx1숫자반복문12 {
+
+	public static void main(String[] args) {
+		/* 1부터 5까지 출력하는 코드를 for문으로 작성하세요.
+		 * 반복횟수 : 5번. 1부터 5까지(같다를 포함) 1씩 증가
+		 * 출력예시
+		 * 1
+		 * 2
+		 * 3
+		 * 4
+		 * 5
+		 * */
+		int num;
+		for(num = 1 ; num <= 5 ; num++ ) {
+		System.out.println(num);
+		}
+	}
+
+}
