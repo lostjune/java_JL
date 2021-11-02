ca573c05ac832420e73406d6c66ef14cb5aaaf7e
java_JL <lostjune@naver.com>
2021-11-02 17:39:39 +0900

    ArrayEx1배열선언16(2021.11.02)

---
+++ JL/src/day5/ArrayEx1배열선언16.java
@@ -0,0 +1,32 @@
+package day5;
+
+public class ArrayEx1배열선언16 {
+
+	public static void main(String[] args) {
+		/* 정수형 배열 선언. 크기는 5개짜리
+		 * 
+		 * */
+		//배열 선언 후, 나중에 배열을 생성하는 방법
+		int arr1 [];
+		arr1 = new int[5];
+		//배열 선언 후, 바로 배열을 생성하는 방법
+		int arr2 [] = new int[5];
+		
+		int []arr3;
+		arr3 = new int[5];
+		int [] arr4 = new int[5];
+		//배열의 크기를 변수로 설정하는 방법
+		int size = 5;
+		int arr5[] = new int[size];
+		
+		//[]을 앞에 쓴 경우와 뒤에 쓴 경우의 차이 (배열을 동시에 만들 경우)
+		int [] arr6, arr7;//방법1) arr6:배열, arr7: 배열
+		//방법2)
+		//int []arr6;
+		//int []arr7;
+		int arr8[], arr9;//방법1) arr8:배열, arr9:변수
+		//방법2)
+		//int arr8[];
+		//intarr9;
+	}
+}
