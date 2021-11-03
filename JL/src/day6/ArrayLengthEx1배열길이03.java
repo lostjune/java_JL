14949f740429c5ea98c2152981e40f0f1b5bc178
lostjune <93233438+lostjune@users.noreply.github.com>
2021-11-03 17:24:09 +0900

    ArrayLengthEx1배열길이03(2021.11.03)

---
+++ JL/src/day6/ArrayLengthEx1배열길이03.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class ArrayLengthEx1배열길이03 {
+
+	public static void main(String[] args) {
+		/*
+		 * 배열의 길이
+		 * 
+		 * */
+		int [] arr = new int [5];
+		System.out.println("배열의 길이 : " + arr.length);
+		int i;
+		for(i = 0; i<arr.length ; i++) {
+			System.out.println("arr["+i+"]=" + arr[i]);
+		}
+		
+	}
+
+}
