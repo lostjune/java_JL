829d674a610458ab8c76cb3be0c2fcad51ae403c
lostjune <93233438+lostjune@users.noreply.github.com>
2021-11-11 17:26:55 +0900

    상속도형1-4(2021.11.11)

---
+++ JL/src/day12/ExaTestEx1.java
@@ -0,0 +1,38 @@
+package day12;
+
+public class ExaTestEx1 {
+
+	public static void main(String[] args) {
+		/*
+		ExaShape s1 = new ExaShape(0,0,10,10);
+		s1.print();
+		ExaShape s2 = new ExaShape(10,10,0,0);
+		s2.print();
+		s2.move(10, 10);
+		s2.print();
+		s2.rerize(20, 20, 3);
+		s2.print();
+		s2.rerize(10,10,2);
+		s2.print();
+		s2.rerize(5,5,1);
+		s2.print();
+		s2.rerize(3,3,4);
+		s2.print();
+		*/
+		/*
+		ExaRect r1 = new ExaRect(0, 0 , 10, 10);
+		r1.print();
+		r1.move(10, 10);
+		r1.print();
+		r1.rerize(15, 20, 1);
+		r1.print();
+		*/
+		ExaEllipse e1 = new ExaEllipse(0, 0 , 10, 10);
+		e1.print();
+		e1.move(10, 10);
+		e1.print();
+		e1.rerize(15, 20, 1);
+		e1.print();
+	}
+
+}
