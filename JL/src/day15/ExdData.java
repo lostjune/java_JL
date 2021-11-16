e67b81c2caf0c7feee754c146601e3cac0859f2f
java_JL <lostjune@naver.com>
2021-11-16 16:59:44 +0900

    제네릭 클래스 4-1(2021.11.16)

---
+++ JL/src/day15/ExdData.java
@@ -0,0 +1,29 @@
+package day15;
+//
+//제네릭스 사용시
+//제네릭 타입으로 올 수 있는 클래스 T는 Number 클래스를 상속받는 클래스만 가능
+//?는 와일드카드라고 부름
+//
+public class ExdData<T extends Number> {
+T data;
+	
+	public void print() {
+		System.out.println(data.getClass().getName() + " : " + data);
+	}
+	public ExdData(T data) {
+		this.data = data;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public void test(ExdData<?> data) {
+		System.out.println(data);
+	}
+}
+
+
+
+
